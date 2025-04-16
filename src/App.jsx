3a1318e7a3daf362c79c993fbc6f331ef0264b1c
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-04-16 19:38:13 +0200

    Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-// Milestone 2: Implementare il Debounce per Ottimizzare la Ricerca
-// Attualmente, ogni pressione di tasto esegue una richiesta API. Questo è inefficiente!
-// Implementa una funzione di debounce per ritardare la chiamata API fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
-// Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
+// Bonus: Caricare i Dettagli del Prodotto Selezionato
+// Quando l’utente clicca su un prodotto nella tendina, nascondi la tendina e carica i dettagli completi del prodotto sotto il campo di ricerca.
 
-// Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
+// Effettua una richiesta API per ottenere i dettagli completi:
+// https://boolean-spec-frontend.vercel.app/freetestapi/products/{id}
+
+// Mostra i dettagli del prodotto selezionato (es. image, name, description, price).
+
+
+// Obiettivo: Aggiungere interattività permettendo di visualizzare le informazioni complete di un prodotto.
 
 function debounce(callback, delay) {
   let timer;
@@ -21,6 +25,7 @@ function App() {
 
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   const fetchProducts = async (query) => {
     if (!query.trim()) {
@@ -45,6 +50,18 @@ function App() {
     debouncedFetchProducts(query);
   }, [query]);
 
+  const fetchProductsDetails = async (id) => {
+    try {
+      const res = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products/${id}`);
+      const data = await res.json();
+      setSelectedProduct(data);
+      setQuery("");
+      setSuggestions([]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <>
       <h1>Autocomplete</h1>
@@ -56,8 +73,16 @@ function App() {
       {suggestions.length > 0 && (
         <div className='dropdown'>
           {suggestions.map((product) => (
-            <p key={product.id}>{product.name}</p>
+            <p key={product.id} onClick={() => fetchProductsDetails(product.id)}>{product.name}</p>
           ))}
+        </div>
+      )}
+      {selectedProduct && (
+        <div className="card">
+          <h2>{selectedProduct.name}</h2>
+          <img src={selectedProduct.image} alt={selectedProduct.name} />
+          <p>{selectedProduct.description}</p>
+          <p><strong>Prezzo:</strong> {selectedProduct.price}€</p>
         </div>
       )}
     </>
